0beef4d75fc2673e1cb7b2c2b4a04c48efd80a4a
krasnovss91 <krasnovstanislav91@gmail.com>
2022-08-25 15:02:03 +0400

    Dto

---
+++ src/main/java/com/dto/Status.java
@@ -0,0 +1,7 @@
+package com.dto;
+
+public enum Status {
+    NEW,
+    RENDERING,
+    COMPLETE
+}
--- src/main/java/com/dto/Task.java
+++ src/main/java/com/dto/Task.java
@@ -1,4 +1,20 @@
 package com.dto;
 
+import lombok.*;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private Status status;
 }
